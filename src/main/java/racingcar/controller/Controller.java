3faf30c52ccecf08af0cac61a6be9592682ab5fa
specture258 <specture258@hanyang.ac.kr>
2024-08-08 00:16:32 +0900

    init(), run(), end() 전체 흐름제어

---
+++ src/main/java/racingcar/controller/Controller.java
@@ -0,0 +1,105 @@
+package racingcar.controller;
+
+import racingcar.domain.Car;
+import racingcar.domain.RandomNumber;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class Controller {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private Integer TRY_COUNT = 0;
+    private List<Car> carObject;
+    private final List<String> winnersList = new ArrayList<>();
+    private final List<Integer> positionList = new ArrayList<>();
+
+    public Controller(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void race(){
+        init();
+        run();
+        end();
+    }
+
+    public void init() {
+        outputView.inputCarName();
+        carObject = getCarObject(inputView.inputCarName());
+
+        outputView.inputTryCount();
+        TRY_COUNT = getTryCount(inputView.inputTryCount());
+    }
+
+    public void run(){
+        outputView.result();
+        while (TRY_COUNT>0) {
+            getCarMoving();
+            TRY_COUNT--;
+            outputView.lineBreak();
+        }
+    }
+
+    public void end(){
+        outputView.finalWinner();
+        getFinalWinner();
+    }
+
+    public List<Car> getCarObject(String cars){
+        List<Car> carObject = new ArrayList<>();
+        String[] carArray = cars.split(",");
+        for(String carName : carArray){
+            Car car = new Car(carName);
+            carObject.add(car);
+        }
+        return carObject;
+    }
+
+    public int getTryCount(String tryCount){
+        return Integer.parseInt(tryCount);
+    }
+
+    public void getCarMoving(){
+        for (Car car : carObject) {
+            outputView.carList(car);
+            createCarMoving(car);
+        }
+    }
+
+    public void createCarMoving(Car car){
+        RandomNumber randomNumber = new RandomNumber();
+        if(randomNumber.canMoving()){
+            outputView.move(car);
+            car.updatePosition();
+        }
+        else{
+            outputView.status(car);
+        }
+    }
+
+    public void getFinalWinner(){
+        for (Car car : carObject) {
+            positionList.add(car.getPosition());
+        }
+        createFinalWinner(Collections.max(positionList));
+        outputView.finalWinnerName(String.join(", ", winnersList));
+    }
+
+    private void createFinalWinner(Integer maxPosition) {
+        for(int i=0 ; i<positionList.size() ;i++){
+            Car car = carObject.get(i);
+            if(positionList.get(i).equals(maxPosition)){
+                winnersList.add(car.toString());
+            }
+        }
+    }
+
+
+}
