a2c928c6f12c7dd8af99de85da41d21cd7e4952a
specture258 <specture258@hanyang.ac.kr>
2024-08-08 00:17:51 +0900

    이동을 결정하는 randomNumber 생성

---
+++ src/main/java/racingcar/domain/RandomNumber.java
@@ -0,0 +1,21 @@
+package racingcar.domain;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
+public class RandomNumber {
+
+    private int MIN_RANGE = 0;
+    private int MAX_RANGE = 9;
+    private int THRESHOLD = 4;
+
+    public int createRandomNumber() {
+       return pickNumberInRange(MIN_RANGE, MAX_RANGE);
+    }
+
+    public boolean canMoving(){
+        if(createRandomNumber() >= THRESHOLD){
+            return true;
+        }
+        return false;
+    }
+}
