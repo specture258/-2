fe6032beeff7e5ed7eb71f5588a992a2781ee35e
specture258 <specture258@hanyang.ac.kr>
2024-08-08 00:18:39 +0900

    Car 객체(위치 업데이트하는 매서드, 이동하는 매서드, 현재 위치 나타내는 매서드 추가)

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,36 @@
+package racingcar.domain;
+
+public class Car {
+    private final String name;
+    private int position = 0;
+    private String statue = "";
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public void updatePosition(){
+        this.position++;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String getStatue() {
+        return statue;
+    }
+
+    public String moving(){
+       return statue = getStatue() + "-";
+    }
+
+
+
+
+}
