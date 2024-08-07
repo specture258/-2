0084874905b03e4ca7a021ea9edc6495fdfe162e
specture258 <specture258@hanyang.ac.kr>
2024-08-08 00:18:52 +0900

    Application

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import racingcar.controller.Controller;
+import racingcar.view.InputView;
+import racingcar.view.InputViewValidation;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 구현 진행
+
+        Controller game = new Controller(new InputView(new InputViewValidation()), new OutputView());
+        game.race();
     }
 }
