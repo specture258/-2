62bcbddadeb66871b956ce63e7fba586c255f51a
specture258 <specture258@hanyang.ac.kr>
2024-08-19 23:02:42 +0900

    출력값 매서드 static으로 바꾸기

---
+++ src/main/java/racingcar/domain/TryCount.java
@@ -0,0 +1,29 @@
+package racingcar.domain;
+
+import racingcar.view.InputView;
+
+public class TryCount {
+
+    private int tryCount = 0;
+    private final InputView inputView ;
+
+    public TryCount(InputView inputView) {
+        this.inputView = inputView;
+    }
+
+    public void createTryCount(){
+        tryCount = Integer.parseInt(inputView.inputTryCount());
+    }
+
+    public int getTryCount() {
+        return tryCount;
+    }
+
+    public void decreaseCount() {
+        tryCount -=1;
+    }
+
+
+
+
+}
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,39 +4,39 @@ import racingcar.domain.Car;
 
 public class OutputView {
 
-    public void inputCarName(){
+    public static void inputCarName(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
-    public void inputTryCount(){
+    public static void inputTryCount(){
         System.out.println("시도할 회수는 몇회인가요?");
     }
 
-    public void printResult(){
+    public static void printResult(){
         System.out.println("실행 결과");
     }
 
-    public void printCarList(Car car){
+    public static void printCarList(Car car){
         System.out.print(car.toString() + " : ");
     }
 
-    public void printStatus(Car car){
+    public static void printStatus(Car car){
         System.out.println(car.getStatue());
     }
 
-    public void printMoving(Car car){
+    public static void printMoving(Car car){
         System.out.println(car.move());
     }
 
-    public void printLineBreak(){
+    public static void printLineBreak(){
         System.out.println();
     }
 
-    public void printFinalWinner(){
+    public static void printFinalWinner(){
         System.out.print("최종 우승자 : ");
     }
 
-    public void printFinalWinnerName(String winners){
+    public static void printFinalWinnerName(String winners){
         System.out.print(winners);
     }
 
